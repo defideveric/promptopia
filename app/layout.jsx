06489512be6828a5065c1a0c918a40ac54e7e776
erichudson <defideveric@gmail.com>
2023-10-03 11:37:39 -0400

    Google auth in progress

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,15 +12,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
         <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
-                
-                <main className='app'>
-                    <NavBar/>
-                    {children}
-                </main>
-                
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'/>
+                </div>
+                    
+                    <main className='app'>
+                        <NavBar/>
+                        {children}
+                    </main>
+            </Provider>      
         </body>
     </html>
   )
