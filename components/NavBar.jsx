001d87f52db3e36494e7245691336fa2a43b656a
erichudson <defideveric@gmail.com>
2023-10-10 16:58:14 -0400

    next config update to render google account image

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -74,7 +74,7 @@ const NavBar = () => {
             {session?.user ? (
                 <div className='flex'>
                     <Image 
-                        src="/assets/images/logo.svg"
+                        src={session?.user.image}
                         width={37}
                         height={37}
                         className='rounded-full'
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
-}
-module.exports = nextConfig
+   images: {
+      domains: ['lh3.googleusercontent.com'],
+   },
+};
+
+module.exports = nextConfig;
